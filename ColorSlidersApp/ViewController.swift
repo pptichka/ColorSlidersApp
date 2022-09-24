56a8c0aee8c666eacb583851136218ad05b7bd02
pptichka <taleskina@gmail.com>
2022-09-24 17:29:24 +0300

    Initial Commit

---
+++ ColorSlidersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorSlidersApp
+//
+//  Created by Ксн Тлскн on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

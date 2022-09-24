c474f75240f236f1421e3a4d00b6ae16f1a93d6d
pptichka <taleskina@gmail.com>
2022-09-24 19:42:53 +0300

    All view objects are placed

--- ColorSlidersApp/ViewController.swift
+++ ColorSlidersApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var clorView: UIView!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func redSliderIsTapped(_ sender: UISlider) {
+    }
+
+    @IBAction func greenSliderIsTapped(_ sender: UISlider) {
+    }
+    
+    @IBAction func blueSliderIsTapped(_ sender: UISlider) {
+    }
 }
 
